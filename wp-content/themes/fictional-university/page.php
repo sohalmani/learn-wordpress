84bab11b0946e844171cb407f9b83cf9b32bd9d6
sohalmani <manisohal837@gmail.com>
2022-11-25 19:10:33 +0530

    apply breadcrumbs functionality

--- wp-content/themes/fictional-university/page.php
+++ wp-content/themes/fictional-university/page.php
@@ -15,12 +15,18 @@
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About
-                    Us</a> <span class="metabox__main">Our History</span>
-            </p>
-        </div>
+        <?php $parent_id = wp_get_post_parent_id(get_the_ID()); ?>
+        <?php if ($parent_id): ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_the_permalink($parent_id); ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i>
+                        <?php echo get_the_title($parent_id); ?>
+                    </a>
+                    <span class="metabox__main"><?php the_title(); ?></span>
+                </p>
+            </div>
+        <?php endif; ?>
 
         <!--        <div class="page-links">-->
         <!--            <h2 class="page-links__title"><a href="#">About Us</a></h2>-->
