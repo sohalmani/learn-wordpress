669105e388e50e77487f6d37425b1616e9d76dc3
sohalmani <manisohal837@gmail.com>
2022-11-25 13:07:13 +0530

    understanding famous loop of wordpress

--- wp-content/themes/fictional-university/index.php
+++ wp-content/themes/fictional-university/index.php
@@ -1,2 +1,13 @@
+<h1><?php bloginfo( 'name' ); ?></h1>
+<p><?php bloginfo( 'description' ); ?></p>
+
 <?php
-echo "This is my amazing theme!";
+while ( have_posts() ): the_post();
+	?>
+    <div>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <p><?php the_content(); ?></p>
+        <hr/>
+    </div>
+    <?php
+endwhile;
