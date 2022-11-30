2301bcbf5eb400638666e753e67282b772d03eeb
sohalmani <manisohal837@gmail.com>
2022-11-30 11:28:47 +0530

    create custom post type 'event'

---
+++ wp-content/mu-plugins/fictional-university-custom-post-types.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Fictional University CPT
+ * Author: Mani Sohal
+ * Version: 1.0.0
+ */
+
+function fictional_university_custom_post_types()
+{
+    register_post_type('event', array('labels' => array('name' => 'Events', 'all_items' => 'All Events', 'add_new_item' => 'Add New Event', 'edit_item' => 'Edit Event', 'singular' => 'Event',), 'menu_icon' => 'dashicons-calendar', 'public' => true,));
+}
+
+add_action('init', 'fictional_university_custom_post_types');
