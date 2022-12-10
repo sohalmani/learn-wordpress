8edbd01ff21fe431a3e4ede482aa206b558a2d5b
sohalmani <manisohal837@gmail.com>
2022-12-10 23:55:12 +0530

    utilise page banner function in single and archive pages

--- wp-content/themes/fictional-university/archive-event.php
+++ wp-content/themes/fictional-university/archive-event.php
@@ -1,15 +1,9 @@
 <?php get_header(); ?>
 
-    <div class="page-banner">
-        <div class="page-banner__bg-image"
-             style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
-            <div class="page-banner__intro">
-                <p><?php the_archive_description(); ?></p>
-            </div>
-        </div>
-    </div>
+    <?php page_banner(array(
+        'title' => 'All Events',
+        'subtitle' => 'See what\'s going on in our world',
+    )); ?>
 
     <div class="container container--narrow page-section">
 		<?php while (have_posts()): the_post(); ?>
@@ -31,7 +25,8 @@
 		<?php endwhile; ?>
 		<?php echo paginate_links(); ?>
         <hr class="section-break">
-        <p>Want to recap the older events? <a href="<?php echo site_url('past-events'); ?>">Check out the past events archive!</a></p>
+        <p>Want to recap the older events? <a href="<?php echo site_url('past-events'); ?>">Check out the past events
+                archive!</a></p>
     </div>
 
 <?php get_footer();
--- wp-content/themes/fictional-university/single-professor.php
+++ wp-content/themes/fictional-university/single-professor.php
@@ -7,23 +7,24 @@ $related_programs = get_field('related_programs');
 <?php while (have_posts()): the_post(); ?>
 	<?php page_banner(); ?>
 
-	<div class="container container--narrow page-section">
-		<div class="generic-content">
+    <div class="container container--narrow page-section">
+        <div class="generic-content">
             <div class="row group">
                 <div class="one-third"><?php the_post_thumbnail('professor-portrait'); ?></div>
                 <div class="two-thirds"><?php the_content(); ?></div>
             </div>
         </div>
 		<?php if ($related_programs): ?>
-			<hr class="section-break">
-			<h2 class="headline headline--medium">Subject(s) Taught</h2>
-			<ul class="link-list min-list">
+            <hr class="section-break">
+            <h2 class="headline headline--medium">Subject(s) Taught</h2>
+            <ul class="link-list min-list">
 				<?php foreach ($related_programs as $program): ?>
-					<li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+                    <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a>
+                    </li>
 				<?php endforeach; ?>
-			</ul>
+            </ul>
 		<?php endif; ?>
-	</div>
+    </div>
 <?php endwhile; ?>
 
 <?php get_footer();
