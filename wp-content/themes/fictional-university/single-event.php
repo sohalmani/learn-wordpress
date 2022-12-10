f1935a4e970c497091c8165e7adb010b5f2c433a
sohalmani <manisohal837@gmail.com>
2022-12-10 12:00:52 +0530

    display upcoming related events in programs as well, a two-way relationship

--- wp-content/themes/fictional-university/single-event.php
+++ wp-content/themes/fictional-university/single-event.php
@@ -1,30 +1,43 @@
+<?php
+$related_programs = get_field('related_programs');
+?>
+
 <?php get_header(); ?>
 
 <?php while (have_posts()): the_post(); ?>
-	<div class="page-banner">
-		<div class="page-banner__bg-image"
-			 style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
-		<div class="page-banner__content container container--narrow">
-			<h1 class="page-banner__title"><?php the_title(); ?></h1>
-			<div class="page-banner__intro">
-				<!-- TODO: Add subtitle below using Custom Fields -->
-				<p>Remind me to edit this field later using Custom Fields.</p>
-			</div>
-		</div>
-	</div>
+    <div class="page-banner">
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <div class="page-banner__intro">
+                <!-- TODO: Add subtitle below using Custom Fields -->
+                <p>Remind me to edit this field later using Custom Fields.</p>
+            </div>
+        </div>
+    </div>
 
-	<div class="container container--narrow page-section">
-		<div class="metabox metabox--position-up metabox--with-home-link">
-			<p>
-				<a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>">
-					<i class="fa fa-home" aria-hidden="true"></i>
-					Event Home
-				</a>
-				<span class="metabox__main">Posted by <?php the_author_posts_link(); ?>. Expires on <?php the_time(); ?></span>
-			</p>
-		</div>
-		<div class="generic-content"><?php the_content(); ?></div>
-	</div>
+    <div class="container container--narrow page-section">
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p>
+                <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>">
+                    <i class="fa fa-home" aria-hidden="true"></i>
+                    Event Home
+                </a>
+                <span class="metabox__main">Posted by <?php the_author_posts_link(); ?>. Expires on <?php the_time(); ?></span>
+            </p>
+        </div>
+        <div class="generic-content"><?php the_content(); ?></div>
+		<?php if ($related_programs): ?>
+            <hr class="section-break">
+            <h2 class="headline headline--medium">Related Program(s)</h2>
+            <ul class="link-list min-list">
+				<?php foreach ($related_programs as $program): ?>
+                    <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+				<?php endforeach; ?>
+            </ul>
+		<?php endif; ?>
+    </div>
 <?php endwhile; ?>
 
 <?php get_footer();
