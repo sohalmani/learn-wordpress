d0c2bbbee049ad59233c9fda332d98d2796ae666
sohalmani <manisohal837@gmail.com>
2022-12-19 20:48:16 +0530

    fetch all posts and separate them before returning

--- wp-content/themes/fictional-university/inc/search-route.php
+++ wp-content/themes/fictional-university/inc/search-route.php
@@ -8,22 +8,64 @@ function custom_search_api() {
 }
 
 function fetch_search_api_result($data) {
-   $professor = new WP_Query(array(
+   $all_posts = new WP_Query(array(
        'posts_per_page' => '-1',
-       'post_type' => 'professor',
-       's' => $data['term']
+       'post_type' => array('post', 'page', 'professor', 'event', 'program'),
+       's' => sanitize_text_field($data['term']),
    ));
 
-   $professor_results = array();
+   $final_results = array(
+       'general_info' => array(),
+       'events' => array(),
+       'professors' => array(),
+       'programs' => array(),
+   );
 
-   while ($professor->have_posts()) {
-       $professor->the_post();
-       array_push($professor_results, array(
-           'title' => get_the_title(),
-           'permalink' => get_the_permalink(),
-       ));
+   while ($all_posts->have_posts()) {
+       $all_posts->the_post();
+
+       switch (get_post_type()) {
+           case 'post':
+               array_push($final_results['general_info'], array(
+                   'title' => get_the_title(),
+                   'permalink' => get_the_permalink(),
+               ));
+               break;
+
+           case 'page':
+               array_push($final_results['general_info'], array(
+                   'title' => get_the_title(),
+                   'permalink' => get_the_permalink(),
+               ));
+               break;
+
+           case 'event':
+               array_push($final_results['events'], array(
+                   'title' => get_the_title(),
+                   'permalink' => get_the_permalink(),
+               ));
+               break;
+
+           case 'professor':
+               array_push($final_results['professors'], array(
+                   'title' => get_the_title(),
+                   'permalink' => get_the_permalink(),
+               ));
+               break;
+
+           case 'program':
+               array_push($final_results['programs'], array(
+                   'title' => get_the_title(),
+                   'permalink' => get_the_permalink(),
+               ));
+               break;
+
+           default:
+               break;
+       }
    }
-   return $professor_results;
+
+   return $final_results;
 }
 
 add_action('rest_api_init', 'custom_search_api');
